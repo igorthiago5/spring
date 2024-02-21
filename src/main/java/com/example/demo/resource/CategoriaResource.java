18c1137ec46ccfa76b966c184982d1b5d2871908
igorthiago5 <igorthiago255@hotmail.com>
2024-02-21 00:52:59 -0300

    Testando a Categoria

--- src/main/java/com/example/demo/resource/CategoriaResource.java
+++ src/main/java/com/example/demo/resource/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.example.demo.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+	private Categoria c1,c2;
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Ola mundo";
+	public List<Categoria> listar() {
+		List<Categoria> list = new ArrayList<>();
+		c1 = new Categoria(1,"Informatica");
+		c2 = new Categoria(2,"Escritotio");
+		list.add(c1);
+		list.add(c2);
+		return list;
 	}
 
 }
